9b8a8eca546ed38a763184e5761f35b65db6313e
Olivia Kaye Minier <ominier@purdue.edu>
2024-09-05 10:37:10 -0400

    original code

--- CGT 215 Lab 4/CGT 215 Lab 4.cpp
+++ CGT 215 Lab 4/CGT 215 Lab 4.cpp
@@ -3,9 +3,53 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+//Print out the menu of choices for the user to select from
+void printMenu() {
+    cout << "Please Select which operation to perform:" << endl;
+    cout << "\t1. Factorial" << endl;
+    cout << "\t2. Arithmetic Series" << endl;
+    cout << "\t3. Geometric Series" << endl;
+    cout << "\t4. Exit" << endl;
+    cout << "Your Selection: ";
+}
+void factorial() {
+    // I recommend writing your factorial code here
+}
+void arithmetic() {
+    // I recommend writing your arithmetic series code here
+}
+void geometric() {
+    // I recommend writing your geometric series code here
+}
+int main() {
+    int choice;
+    char again;
+
+    do {
+        printMenu();
+        cin >> choice;
+
+        // Quit if user chooses to exit (for any invalid choice)
+        if (choice > 3 || choice < 1) {
+            return 0;
+        }
+
+        else if (choice == 1) {
+            factorial();
+        }
+        else if (choice == 2) {
+            arithmetic();
+        }
+        else if (choice == 3) {
+            geometric();
+        }
+
+        cout << "Go Again? [Y/N] ";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y"');
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
